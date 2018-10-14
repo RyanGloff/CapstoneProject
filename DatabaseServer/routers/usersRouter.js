9b479e22fb7591215734263ef15638b4a5508f02
Ryan Gloff <ryan.gloff@yahoo.com>
2018-10-14 19:05:41 -0400

    Fixed postgres push

--- DatabaseServer/routers/usersRouter.js
+++ DatabaseServer/routers/usersRouter.js
@@ -1,10 +1,10 @@
 function addEndpoints (app, restAuditer, dbConnector) {
-    app.get('/users', (req, res) => {
+    app.get('/users', async (req, res) => {
         console.log('GET\t/users\tResponse: 200');
-        res.status(200).send(dbConnector.getUsers());
+        res.status(200).send(await dbConnector.getUsers());
     });
-    app.get('/users/:id', (req, res) => {
-        let user = dbConnector.getUserById(parseInt(req.params.id));
+    app.get('/users/:id', async (req, res) => {
+        let user = await dbConnector.getUserById(parseInt(req.params.id));
         if (user === undefined) {
             console.log('GET\t/users/' + req.params.id + '\tResponse: 404');
             res.sendStatus(404);
@@ -13,39 +13,33 @@ function addEndpoints (app, restAuditer, dbConnector) {
             res.status(200).send(user);
         }
     });
-    app.post('/users', (req, res) => {
+    app.post('/users', async (req, res) => {
         let auditRes = restAuditer.auditAddUser(req.body);
-        let dbRes = dbConnector.addUser(req.body);
-        if (dbRes.success && auditRes) {
-            console.log('POST\t/users\tResponse: 200');
-            res.status(200).send({id: dbRes.id});
-        } else if (!auditRes) {
+        let dbRes = await dbConnector.addUser(req.body);
+        if (!auditRes) {
             console.log('POST\t/users\tResponse: ' + 400);
             res.status(400).send('Malformed payload');
+            return;
+        }
+        console.log(dbRes);
+        if (dbRes.success) {
+            console.log('POST\t/users\tResponse: 200');
+            res.status(200).send({id: dbRes.id});
         } else {
             console.log('POST\t/users\tResponse: ' + dbRes.reason);
             res.sendStatus(dbRes.reason);
         }
     });
-    app.delete('/users', (req, res) => {
-        let dbRes = dbConnector.deleteAllUsers();
-        if (dbRes.success) {
-            console.log('DELETE\t/users\tResponse: 200');
-            res.sendStatus(200);
-        } else {
-            console.log('DELETE\t/users\tResponse: 500');
-            res.sendStatus(500);
-        }
+    app.delete('/users', async (req, res) => {
+        await dbConnector.deleteAllUsers();
+        console.log('DELETE\t/users\tResponse: 200');
+        res.sendStatus(200);
+
     });
-    app.delete('/users/:id', (req, res) => {
-        let dbRes = dbConnector.deleteUser(parseInt(req.params.id));
-        if (dbRes.success) {
-            console.log('DELETE\t/users/' + req.params.id + '\tResponse: 200');
-            res.sendStatus(200);
-        } else {
-            console.log('DELETE\t/users/' + req.params.id + '\tResponse: ' + dbRes.reason);
-            res.sendStatus(dbRes.reason);
-        }
+    app.delete('/users/:id', async (req, res) => {
+        await dbConnector.deleteUser(parseInt(req.params.id));
+        console.log('DELETE\t/users/' + req.params.id + '\tResponse: 200');
+        res.sendStatus(200);
     });
 }
 
--- DatabaseServer/server.js
+++ DatabaseServer/server.js
@@ -18,68 +18,8 @@ const loginRouter = require('./routers/loginRouter');
 
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
-// /Users
-// GET operations
-app.get('/users', async (req, res) => {
-    console.log('GET\t/users\tResponse: 200');
-    res.status(200).send(await dbConnector.getUsers());
-});
-app.get('/users/:id', async (req, res) => {
-    let user = await dbConnector.getUserById(parseInt(req.params.id));
-    if (user === undefined) {
-        console.log('GET\t/users/' + req.params.id + '\tResponse: 404');
-        res.sendStatus(404);
-    } else {
-        console.log('GET\t/users/' + req.params.id + '\tResponse: 200');
-        res.status(200).send(user);
-    }
-});
-
-// POST operations
-app.post('/users', async (req, res) => {
-    let auditRes = restAuditer.auditAddUser(req.body);
-    let dbRes = await dbConnector.addUser(req.body);
-    if (dbRes.success && auditRes) {
-        console.log('POST\t/users\tResponse: 200');
-        res.status(200).send({id: dbRes.id});
-    } else if (!auditRes) {
-        console.log('POST\t/users\tResponse: ' + 400);
-        res.status(400).send('Malformed payload');
-    } else {
-        console.log('POST\t/users\tResponse: ' + dbRes.reason);
-        res.sendStatus(dbRes.reason);
-    }
-});
-
-// DELETE operations
-app.delete('/users', (req, res) => {
-    let dbRes = dbConnector.deleteAllUsers();
-    console.log('DELETE\t/users\tResponse: 200');
-    res.sendStatus(200);
-});
-app.delete('/users/:id', (req, res) => {
-    let dbRes = dbConnector.deleteUser(parseInt(req.params.id));
-	console.log('DELETE\t/users/' + req.params.id + '\tResponse: 200');
-	res.sendStatus(200);
-});
-
-// /login
-app.post('/login', async function(req, res) {
-    let dbRes = await dbConnector.areValidCredentials(req.body);
-	console.log(dbRes)
-    if (dbRes) {
-        console.log('POST\t/login\tResponse: 200');
-        res.sendStatus(200);
-    } else {
-        console.log('POST\t/login\tResponse: 401');
-        res.sendStatus(401);
-    }
-});
-=======
 usersRouter.addEndpoints(app, restAuditer, dbConnector);
 gamesRouter.addEndpoints(app, restAuditer, dbConnector);
 loginRouter.addEndpoints(app, restAuditer, dbConnector);
->>>>>>> e3608f618d516e35b2a26613db6e42ff32794c2b
 
 server.listen(8081);
