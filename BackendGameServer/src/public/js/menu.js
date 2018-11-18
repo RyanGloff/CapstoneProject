5fead346ca9d78de640828eb6106bd3eff5886d1
Nick Kowalchyk <nickkowalchyk@gmail.com>
2018-11-18 16:51:47 -0500

    Menu added to html

---
+++ BackendGameServer/src/public/js/menu.js
@@ -0,0 +1,9 @@
+const menuContainer = document.getElementById('menu');
+
+function toggleMenu() {
+    menuContainer.style.display = 'block';
+}
+
+function hideMenu() {
+    menuContainer.style.display = 'none';
+}
